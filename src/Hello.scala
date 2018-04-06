680af066f430545af22e28e193c53bdff2d93e4c
Dmitry Kaplunov <dmitriy.kaplunov@center-it.com>
2018-04-06 13:11:05 +0500

    Fix RegEx

--- src/Hello.scala
+++ src/Hello.scala
@@ -1,15 +1,15 @@
 object Hello extends App {
-  var str = "(123+456)*789-sin(10)"//scala.io.StdIn.readLine()
+  var str = "(123+456)*789 - sin (10)"//scala.io.StdIn.readLine()
 
   val re = """\d+|(\w+\s*\()|\(|\)|\+|\*|\/|\-""".r
-  val reF = """(\w+\s*\()|\(|\)|\+|\*|\/|\-""".r
+  val reF = """(\w+)|\(|\)|\+|\*|\/|\-""".r
   val reO = """\d+""".r
 
   def isNumeric (item:String) = scala.util.Try(item.toDouble).isSuccess
 
 //  re.findAllIn(str).foreach(item => println(item+" -- "+isNumeric(item)))
 
-//  reF.findAllIn(str).foreach(item => println(item))
+  reF.findAllIn(str).foreach(item => println(item))
 
 //  reO.findAllIn(str).foreach(item => println(item))
 
